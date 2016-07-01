a07c6cb9dc32b8b6c5d13d3412d655b2b2ad19d7
LumberZack <zack.poirier88@gmail.com>
2016-07-01 14:18:39 -0400

    Functionally Complete
    Did all the work.
    Bad Git practice I know, but it only took 40 mins.

--- CourtCounterApp/app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ CourtCounterApp/app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.android.courtcounter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,84 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    int homeScoreValue = 0;
+    int awayScoreValue = 0;
+
+    public void home1PT(View view){
+        TextView homeScore = (TextView) findViewById(R.id.homePoints);
+        homeScoreValue = homeScoreValue + 1;
+        if (homeScoreValue < 10){
+            homeScore.setText("0" + homeScoreValue);
+        }
+        else {
+            homeScore.setText("" + homeScoreValue);
+        }
+    }
+
+    public void home2PT(View view){
+        TextView homeScore = (TextView) findViewById(R.id.homePoints);
+        homeScoreValue = homeScoreValue + 2;
+        if (homeScoreValue < 10){
+            homeScore.setText("0" + homeScoreValue);
+        }
+        else {
+            homeScore.setText("" + homeScoreValue);
+        }
+    }
+
+    public void home3PT(View view){
+        TextView homeScore = (TextView) findViewById(R.id.homePoints);
+        homeScoreValue = homeScoreValue + 3;
+        if (homeScoreValue < 10){
+            homeScore.setText("0" + homeScoreValue);
+        }
+        else {
+            homeScore.setText("" + homeScoreValue);
+        }
+    }
+
+    public void away1PT(View view){
+        TextView awayScore = (TextView) findViewById(R.id.awayPoints);
+        awayScoreValue = awayScoreValue + 1;
+        if (awayScoreValue < 10){
+            awayScore.setText("0" + awayScoreValue);
+        }
+        else {
+            awayScore.setText("" + awayScoreValue);
+        }
+    }
+
+    public void away2PT(View view){
+        TextView awayScore = (TextView) findViewById(R.id.awayPoints);
+        awayScoreValue = awayScoreValue + 2;
+        if (awayScoreValue < 10){
+            awayScore.setText("0" + awayScoreValue);
+        }
+        else {
+            awayScore.setText("" + awayScoreValue);
+        }
+    }
+
+    public void away3PT(View view){
+        TextView awayScore = (TextView) findViewById(R.id.awayPoints);
+        awayScoreValue = awayScoreValue + 3;
+        if (awayScoreValue < 10){
+            awayScore.setText("0" + awayScoreValue);
+        }
+        else {
+            awayScore.setText("" + awayScoreValue);
+        }
+    }
+
+    public void reset(View view){
+        TextView homeScore = (TextView) findViewById(R.id.homePoints);
+        homeScoreValue = 0;
+        homeScore.setText("0" + homeScoreValue);
+
+        TextView awayScore = (TextView) findViewById(R.id.awayPoints);
+        awayScoreValue = 0;
+        awayScore.setText("0" + awayScoreValue);
     }
 }
